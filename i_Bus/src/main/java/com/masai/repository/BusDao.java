8bfd99b01bec3fb2113551285338d1585ab02725
Lalit Singh <103200871+Lalitsingh28@users.noreply.github.com>
2022-11-11 20:48:33 +0530

    Delete i_Bus directory

--- i_Bus/src/main/java/com/masai/repository/BusDao.java
+++
@@ -1,9 +0,0 @@
-package com.masai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.models.Bus;
-
-public interface BusDao extends JpaRepository<Bus, Integer>{
-
-}
